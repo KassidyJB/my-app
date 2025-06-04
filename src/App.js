57423043d33a669638c888da30c1908d191ae18a
KassidyJB <jeanbartkassidy51@gmail.com>
2025-06-03 23:58:59 -0400

    edits to screen display

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Edit <code>src/App.js</code> and save to reload.
+      </header>
+      <p>
+        My name is Kassidy Jeanbart, I'll be an intern at CPAL 
+        this summer.
+      </p>
+      <section>
+        This is a test site for project submission
+      </section>
+    </div>
+  );
+}
+
+export default App;
